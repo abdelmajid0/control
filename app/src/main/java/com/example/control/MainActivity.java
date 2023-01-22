0336b429729b5e8d087596678858144602cf2c94
AbdelmajidAlouah <abdelmajid@louah.com>
2023-01-22 07:00:08 +0100

    Button_Reinitialiser

--- app/src/main/java/com/example/control/MainActivity.java
+++ app/src/main/java/com/example/control/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.control;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView editText1;
+    Button btn1,btn2,btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        editText1 = findViewById(R.id.editText1);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText("");
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
     }
+
+
 }
