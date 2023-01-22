c29171e09c25837f0f22922a9656e35ff083c190
AbdelmajidAlouah <abdelmajid@louah.com>
2023-01-22 14:01:51 +0100

    Bug-fixed(button-reinesialistion&AffechageView&AppLabelChenges)

--- app/src/main/java/com/example/control/MainActivity.java
+++ app/src/main/java/com/example/control/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.TreeSet;
@@ -12,6 +13,7 @@ import java.util.TreeSet;
 public class MainActivity extends AppCompatActivity {
     TextView editText1;
     Button btn1,btn2,btn3;
+    LinearLayout layoutview1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +24,21 @@ public class MainActivity extends AppCompatActivity {
         btn2 = findViewById(R.id.button2);
         btn3 = findViewById(R.id.button3);
         editText1 = findViewById(R.id.editText1);
+        layoutview1 = findViewById(R.id.layoutview1);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editText1.setText("");
+                for(int loop = 0; loop < layoutview1.getChildCount(); loop++) {
+                    //this is the code in question and where I want to get the text from
+                    //all my EditTexts
+                    TextView inner = (TextView) layoutview1.getChildAt(loop);
+                    inner.setText("? *"+String.format(" %s ",loop+1)+"= ?");
+
+
+                }
             }
         });
 
